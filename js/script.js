6d7ebd3378c1b645f7be7b84afee709b4ff85fa9
Chrissszz <c.barrett228@yahoo.com>
2024-11-18 18:16:23 -0600

    images

--- js/script.js
+++ js/script.js
@@ -1,59 +1,59 @@
 //Hearts
-let cardAceHearts = [1, "Ace", "Hearts"];
-let cardTwoHearts = [2, "Two", "Hearts"];
-let cardThreeHearts = [3, "Three", "Hearts"];
-let cardFourHearts = [4, "Four", "Hearts"];
-let cardFiveHearts = [5, "Five", "Hearts"];
-let cardSixHearts = [6, "Six", "Hearts"];
-let cardSevenHearts = [7, "Seven", "Hearts"];
-let cardEightHearts = [8, "Eight", "Hearts"];
-let cardNineHearts = [9, "Nine", "Hearts"];
-let cardTenHearts = [10, "Ten", "Hearts"];
-let cardJackHearts = [10, "Jack", "Hearts"];
-let cardQueenHearts = [10, "Queen", "Hearts"];
-let cardKingHearts = [10, "King", "Hearts"];
+let cardAceHearts = [1, "Ace", "Hearts", "images/cards/hearts/hearts_A.png"];
+let cardTwoHearts = [2, "Two", "Hearts", "images/cards/hearts/hearts_2.png"];
+let cardThreeHearts = [3, "Three", "Hearts", "images/cards/hearts/hearts_3.png"];
+let cardFourHearts = [4, "Four", "Hearts", "images/cards/hearts/hearts_4.png"];
+let cardFiveHearts = [5, "Five", "Hearts", "images/cards/hearts/hearts_5.png"];
+let cardSixHearts = [6, "Six", "Hearts", "images/cards/hearts/hearts_6.png"];
+let cardSevenHearts = [7, "Seven", "Hearts", "images/cards/hearts/hearts_7.png"];
+let cardEightHearts = [8, "Eight", "Hearts", "images/cards/hearts/hearts_8.png"];
+let cardNineHearts = [9, "Nine", "Hearts", "images/cards/hearts/hearts_9.png"];
+let cardTenHearts = [10, "Ten", "Hearts", "images/cards/hearts/hearts_10.png"];
+let cardJackHearts = [10, "Jack", "Hearts", "images/cards/hearts/hearts_J.png"];
+let cardQueenHearts = [10, "Queen", "Hearts", "images/cards/hearts/hearts_Q.png"];
+let cardKingHearts = [10, "King", "Hearts", "images/cards/hearts/hearts_K.png"];
 //Diamonds
-let cardAceDiamonds = [11, "Ace", "Diamonds"];
-let cardTwoDiamonds = [2, "Two", "Diamonds"];
-let cardThreeDiamonds = [3, "Three", "Diamonds"];
-let cardFourDiamonds = [4, "Four", "Diamonds"];
-let cardFiveDiamonds = [5, "Five", "Diamonds"];
-let cardSixDiamonds = [6, "Six", "Diamonds"];
-let cardSevenDiamonds = [7, "Seven", "Diamonds"];
-let cardEightDiamonds = [8, "Eight", "Diamonds"];
-let cardNineDiamonds = [9, "Nine", "Diamonds"];
-let cardTenDiamonds = [10, "Ten", "Diamonds"];
-let cardJackDiamonds = [10, "Jack", "Diamonds"];
-let cardQueenDiamonds = [10, "Queen", "Diamonds"];
-let cardKingDiamonds = [10, "King", "Diamonds"];
+let cardAceDiamonds = [11, "Ace", "Diamonds", 'images/cards/diamonds/diamonds_A.png'];
+let cardTwoDiamonds = [2, "Two", "Diamonds", 'images/cards/diamonds/diamonds_2.png'];
+let cardThreeDiamonds = [3, "Three", "Diamonds", 'images/cards/diamonds/diamonds_3.png'];
+let cardFourDiamonds = [4, "Four", "Diamonds", 'images/cards/diamonds/diamonds_4.png'];
+let cardFiveDiamonds = [5, "Five", "Diamonds", 'images/cards/diamonds/diamonds_5.png'];
+let cardSixDiamonds = [6, "Six", "Diamonds", 'images/cards/diamonds/diamonds_6.png'];
+let cardSevenDiamonds = [7, "Seven", "Diamonds", 'images/cards/diamonds/diamonds_7.png'];
+let cardEightDiamonds = [8, "Eight", "Diamonds", 'images/cards/diamonds/diamonds_8.png'];
+let cardNineDiamonds = [9, "Nine", "Diamonds", 'images/cards/diamonds/diamonds_9.png'];
+let cardTenDiamonds = [10, "Ten", "Diamonds", 'images/cards/diamonds/diamonds_10.png'];
+let cardJackDiamonds = [10, "Jack", "Diamonds", 'images/cards/diamonds/diamonds_J.png'];
+let cardQueenDiamonds = [10, "Queen", "Diamonds", 'images/cards/diamonds/diamonds_Q.png'];
+let cardKingDiamonds = [10, "King", "Diamonds", 'images/cards/diamonds/diamonds_K.png'];
 //Clubs
-let cardAceClubs = [11, "Ace", "Clubs"];
-let cardTwoClubs = [2, "Two", "Clubs"];
-let cardThreeClubs = [3, "Three", "Clubs"];
-let cardFourClubs = [4, "Four", "Clubs"];
-let cardFiveClubs = [5, "Five", "Clubs"];
-let cardSixClubs = [6, "Six", "Clubs"];
-let cardSevenClubs = [7, "Seven", "Clubs"];
-let cardEightClubs = [8, "Eight", "Clubs"];
-let cardNineClubs = [9, "Nine", "Clubs"];
-let cardTenClubs = [10, "Ten", "Clubs"];
-let cardJackClubs = [10, "Jack", "Clubs"];
-let cardQueenClubs = [10, "Queen", "Clubs"];
-let cardKingClubs = [10, "King", "Clubs"];
+let cardAceClubs = [11, "Ace", "Clubs", 'images/cards/clubs/clubs_A.png'];
+let cardTwoClubs = [2, "Two", "Clubs", 'images/cards/clubs/clubs_2.png'];
+let cardThreeClubs = [3, "Three", "Clubs", 'images/cards/clubs/clubs_3.png'];
+let cardFourClubs = [4, "Four", "Clubs", 'images/cards/clubs/clubs_4.png'];
+let cardFiveClubs = [5, "Five", "Clubs", 'images/cards/clubs/clubs_5.png'];
+let cardSixClubs = [6, "Six", "Clubs", 'images/cards/clubs/clubs_6.png'];
+let cardSevenClubs = [7, "Seven", "Clubs", 'images/cards/clubs/clubs_7.png'];
+let cardEightClubs = [8, "Eight", "Clubs", 'images/cards/clubs/clubs_8.png'];
+let cardNineClubs = [9, "Nine", "Clubs", 'images/cards/clubs/clubs_9.png'];
+let cardTenClubs = [10, "Ten", "Clubs", 'images/cards/clubs/clubs_10.png'];
+let cardJackClubs = [10, "Jack", "Clubs", 'images/cards/clubs/clubs_J.png'];
+let cardQueenClubs = [10, "Queen", "Clubs", 'images/cards/clubs/clubs_Q.png'];
+let cardKingClubs = [10, "King", "Clubs", 'images/cards/clubs/clubs_K.png'];
 //Spades
-let cardAceSpades = [11, "Ace", "Spades"];
-let cardTwoSpades = [2, "Two", "Spades"];
-let cardThreeSpades = [3, "Three", "Spades"];
-let cardFourSpades = [4, "Four", "Spades"];
-let cardFiveSpades = [5, "Five", "Spades"];
-let cardSixSpades = [6, "Six", "Spades"];
-let cardSevenSpades = [7, "Seven", "Spades"];
-let cardEightSpades = [8, "Eight", "Spades"];
-let cardNineSpades = [9, "Nine", "Spades"];
-let cardTenSpades = [10, "Ten", "Spades"];
-let cardJackSpades = [10, "Jack", "Spades"];
-let cardQueenSpades = [10, "Queen", "Spades"];
-let cardKingSpades = [10, "King", "Spades"];
+let cardAceSpades = [11, "Ace", "Spades", 'images/cards/spades/spades_A.png'];
+let cardTwoSpades = [2, "Two", "Spades", 'images/cards/spades/spades_2.png'];
+let cardThreeSpades = [3, "Three", "Spades", 'images/cards/spades/spades_3.png'];
+let cardFourSpades = [4, "Four", "Spades", 'images/cards/spades/spades_4.png'];
+let cardFiveSpades = [5, "Five", "Spades", 'images/cards/spades/spades_5.png'];
+let cardSixSpades = [6, "Six", "Spades", 'images/cards/spades/spades_6.png'];
+let cardSevenSpades = [7, "Seven", "Spades", 'images/cards/spades/spades_7.png'];
+let cardEightSpades = [8, "Eight", "Spades", 'images/cards/spades/spades_8.png'];
+let cardNineSpades = [9, "Nine", "Spades", 'images/cards/spades/spades_9.png'];
+let cardTenSpades = [10, "Ten", "Spades", 'images/cards/spades/spades_10.png'];
+let cardJackSpades = [10, "Jack", "Spades", 'images/cards/spades/spades_J.png'];
+let cardQueenSpades = [10, "Queen", "Spades", 'images/cards/spades/spades_Q.png'];
+let cardKingSpades = [10, "King", "Spades", 'images/cards/spades/spades_K.png'];
 
 let allCards = [
     //Hearts
@@ -83,6 +83,8 @@ let hitButton = document.querySelector('#hitButton');
 let standButton = document.querySelector('#standButton');
 let playerOutcome = document.querySelector('.playerOutcome');
 let dealerOutcome = document.querySelector('.dealerOutcome');
+let imageResetDealer = document.querySelector('.cardDisplayImageDealer');
+let imageResetPlayer = document.querySelector('.cardDisplayImagePlayer');
 
 function startGame() {
     playerStartCards();
@@ -101,6 +103,16 @@ function playerStartCards(){
     }
     playerCount = checkForAce(playerCount, playerAceCount);
     cardOutputPlayer.innerHTML = `${randomCard[1]} of ${randomCard[2]}` + `, ${randomCardTwo[1]} of ${randomCardTwo[2]}`;
+    
+    let cardImage = new Image();
+    cardImage.src = randomCard[3]
+    document.querySelector('.cardDisplayImagePlayer').appendChild(cardImage);
+    
+    
+    let cardImageTwo = new Image()
+    cardImageTwo.src = randomCardTwo[3]
+    document.querySelector('.cardDisplayImagePlayer').appendChild(cardImageTwo);
+
     if(playerCount === 21){
         playerOutcome.innerHTML = "Blackjack!";
         endGame();
@@ -117,6 +129,16 @@ function dealerStartCards(){
     }
     dealerCount = checkForAce(dealerCount, dealerAceCount);
     cardOutputDealer.innerHTML = `${randomCard[1]} of ${randomCard[2]}` + `, ${randomCardTwo[1]} of ${randomCardTwo[2]}`;
+    
+    let cardImage = new Image();
+    cardImage.src = randomCard[3]
+    document.querySelector('.cardDisplayImageDealer').appendChild(cardImage);
+    
+    
+    let cardImageTwo = new Image()
+    cardImageTwo.src = randomCardTwo[3]
+    document.querySelector('.cardDisplayImageDealer').appendChild(cardImageTwo);
+
     if(dealerCount === 21){
         dealerOutcome.innerHTML = "Blackjack!";
         endGame();
@@ -131,6 +153,11 @@ function cardCount() {
             playerAceCount++;
         }
         playerCount = checkForAce(playerCount, playerAceCount);
+       
+        let cardImage = new Image();
+        cardImage.src = randomCard[3]
+        document.querySelector('.cardDisplayImagePlayer').appendChild(cardImage);
+
     if(playerCount === 21){
         playerOutcome.innerHTML = "You win!";
         endGame();
@@ -148,6 +175,11 @@ function standCount() {
             dealerAceCount++;
         }
         dealerCount = checkForAce(dealerCount, dealerAceCount);
+
+        let cardImage = new Image();
+        cardImage.src = randomCard[3]
+        document.querySelector('.cardDisplayImageDealer').appendChild(cardImage);
+
         if(dealerCount === 21){
             dealerOutcome.innerHTML = "You win!";
             endGame();
@@ -167,6 +199,8 @@ function restartGame(){
     dealerOutcome.innerHTML = "";
     cardOutputDealer.innerHTML = "";
     cardOutputPlayer.innerHTML = "";
+    imageResetDealer.innerHTML = "";
+    imageResetPlayer.innerHTML = "";
     startButton.disabled = false;
     hitButton.disabled = false;
     standButton.disabled = false;
